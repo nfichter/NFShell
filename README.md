60b81ac72d752d039c1aaabcac8f5815a5d7c797
Noah Fichter <noahfichter@MacBook-Pro.local>
2016-12-02 15:28:50 -0500

    almost done, updating readme

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # NFShell
+
+by Noah Fichter
+
+Features:
+	-Executes single commands ignoring excess whitespace
+	-Executes multiple commands separated by semicolons ignoring excess whitespace
+	-Implements simple redirection using < and >
+	----Can only be used once per single command (can be used multiple times if you use a semicolon)
+	----< can only be used if it is the first argument in a command
+	-Implements simple piping using |
+	----Only one pipe may be used
+		
+Bugs:
+	-Only one < or > may be used, same with pipes
+	
+Functions:
+	-void printPrompt();
+	Inputs: None
+	Returns: None
+	Prints the prompt
+	
+	-void getBuffer(char * retBuff); 
+	Inputs: A char pointer
+	Returns: None
+	Copies the text the user enters into retBuff
+	
+	-char ** splitBySemicolon(char * buffer);
+	Inputs: A char pointer
+	Returns: A double pointer of full commands split by semicolon
+	Splits the full command into an array of commands split by semicolon
+	
+	-char ** splitBySpace(char * buffer);
+	Inputs: A char pointer
+	Returns: A double pointer of full commands split by semicolon
+	Splits a single command into an array of sub-commands split by space
+	
+	-int count(char * str, char toCount);
+	Inputs: A char pointer, and a char
+	Returns: The number of occurrences of toCount in str
+	Counts the number of occurrences of toCount in str
+	
+	-int pipeCheck(char *command);
+	
+	-int redirectCheck(char *command);
+	
+	-int isExit(char *command);
+	
+	-int isCd(char *command);
+	
+	-void exec1(char ** command); 
+	
+	-void piper(char **commands, char *s2); 
+	
+	-void redir(char *s2);
+	
+	-void run(); 
+	
+	-int main();
--- shell.h
+++ shell.h
@@ -1,14 +1,20 @@
-void printPrompt();
-void getBuffer(char * retBuff);
-char ** splitBySemicolon(char * buffer);
-char ** splitBySpace(char * buffer);
-int count(char * str, char toCount);
-int pipeCheck(char *command);
-int redirectCheck(char *command);
-int isExit(char *command);
-int isCd(char *command);
-void exec1(char ** command);
-void piper(char **commands, char *s2);
-void redir(char *s2);
-void run();
-int main();
+void printPrompt(); //prints the prompt
+void getBuffer(char * retBuff); //copies the buffer into the given string pointer
+char ** splitBySemicolon(char * buffer); //splits the buffer string into an array of strings by semicolon,
+										 //each string representing a full command (returns the array of strings)
+char ** splitBySpace(char * buffer); //splits each command string from ^^^ into another array of strings,
+								     //each string representing a single command (returns the array of strings)
+int count(char * str, char toCount); //counts the occurrences of the character toCount in the string str, returns the number
+int pipeCheck(char *command); //checks for the number of pipes in a full command, returns number of pipes found
+int redirectCheck(char *command); //checks for number of redirection symbols ("<" and ">"), returns:
+										//-1 if there is only one occurrence of <
+										//1 if there is only one occurrence of >
+										//0 if there are no occurrences of either
+										//2+ if there are too many of either
+int isExit(char *command); //checks if the command is exit, returns 0 if not, 1 if so
+int isCd(char *command); //checks if the command is cd, returns 0 if not, 1 if so
+void exec1(char ** command); //executes one full command separated by spaces
+void piper(char **commands, char *s2); //side run function, checks for piping and does it
+void redir(char *s2); //bulk of run function, checks for redirection and does it
+void run(); //main running function
+int main(); //just calls run() repeatedly
